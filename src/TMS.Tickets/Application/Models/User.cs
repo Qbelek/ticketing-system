f38b7e410812325723cf73b99b0b84f8233f7013
Qbelek <jakubtomilo@gmail.com>
2020-07-19 16:21:01 +0200

    Added User model class

---
+++ src/TMS.Tickets/Application/Models/User.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace TMS.Tickets.Application.Models
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
