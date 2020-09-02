d16eb120bf3fb2b670a28d3d18bb0006585c1db3
Qbelek <jakubtomilo@gmail.com>
2020-09-02 17:42:24 +0200

    Added GetTicketQuery and its handler for querying for single ticket

---
+++ src/TMS.Tickets.Integration/Queries/GetTicketQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using TMS.Shared;
+using TMS.Shared.DTO;
+
+namespace TMS.Tickets.Integration.Queries
+{
+    public class GetTicketQuery : IRequest<Response<TicketDTO>>
+    {
+        public GetTicketQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
