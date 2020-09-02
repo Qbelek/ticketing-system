613400e7960541adbcfb69c7504f005f53038172
Qbelek <jakubtomilo@gmail.com>
2020-09-02 17:38:33 +0200

    Registered DbContext, Automapper, Mediatr, MachineDateTime in Startup. Disabled default requests validation. Disabled returning null values from the api.

--- src/TMS.Web/Startup.cs
+++ src/TMS.Web/Startup.cs
@@ -1,17 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using TMS.Web.Api;
+using TMS.Shared;
+using TMS.Tickets.Application.Handlers;
+using TMS.Tickets.Config;
+using TMS.Tickets.Integration.Queries;
+using TMS.Tickets.Persistence;
 
 namespace TMS.Web
 {
@@ -27,9 +26,21 @@ namespace TMS.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMediatR(typeof(BaseController).Assembly);
-            
-            services.AddControllers();
+            services.AddDbContextPool<TicketsDbContext>(options =>
+                options.UseInMemoryDatabase("in-memory"));
+
+            services.AddMediatR(
+                typeof(GetTicketQuery).Assembly,
+                typeof(GetTicketQueryHandler).Assembly);
+
+            services.AddAutoMapper(
+                typeof(AutomapperTicketsProfile).Assembly);
+
+            RegisterServices(services);
+
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true)
+                .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +57,15 @@ namespace TMS.Web
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton<MachineDateTime>();
         }
     }
 }
