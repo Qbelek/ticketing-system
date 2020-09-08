f2ee9a418e6442e2dff51ae2752ff6d729c7a425
Qbelek <jakubtomilo@gmail.com>
2020-09-08 19:42:24 +0200

    Added custom binder to enable returning runtime types' properties from the api

--- src/TMS.Shared/DTO/BaseDTO.cs
+++ src/TMS.Shared/DTO/BaseDTO.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace TMS.Shared.DTO
 {
+    [JsonConverter(typeof(RuntimeTypeConverter<BaseDTO>))]
     public abstract class BaseDTO
     {
         public Guid? Id { get; set; }
