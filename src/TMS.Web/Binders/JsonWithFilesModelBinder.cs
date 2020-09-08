f159b0e25228cd7b8d1598d68a5302d6212cf485
Qbelek <jakubtomilo@gmail.com>
2020-09-08 19:33:38 +0200

    Added custom binder to be able to sens json data and files in single request

---
+++ src/TMS.Web/Binders/JsonWithFilesModelBinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TMS.Web.Binders
+{
+    public class JsonWithFilesModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueProviderResult != ValueProviderResult.None)
+            {
+                bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+                var values = valueProviderResult.FirstValue;
+
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                var result = JsonSerializer.Deserialize(values, bindingContext.ModelType, jsonOptions);
+
+                if (result != null)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                    return Task.CompletedTask;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
